c58cb0c72c173ef27feef6d18280f818588f2699
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2018-04-03 16:28:03 +0300

    Implement ninject dependency container

---
+++ Server/BullsAndCows.Web/App_Start/NinjectConfig.cs
@@ -0,0 +1,16 @@
+﻿namespace BullsAndCows.Web
+{
+    using BullsAndCows.Data;
+    using Ninject;
+    using System.Data.Entity;
+
+    public class NinjectConfig
+    {
+        public static void RegisterServices(IKernel kernel)
+        {
+            // e.g. kernel.Load(Assembly.GetExecutingAssembly());
+            kernel.Bind<DbContext>().To<BullsAndCowsDbContext>();
+            kernel.Bind(typeof(IRepository<>)).To(typeof(GenericRepository<>));
+        }
+    }
+}
