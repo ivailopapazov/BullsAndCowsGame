1b5603bef111bda5a9705cafd1fab81cf1e0157e
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2018-04-03 15:11:30 +0300

    Implement repository pattern

---
+++ Server/BullsAndCows.Data/IRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace BullsAndCows.Data
+{
+    using System.Linq;
+
+    public interface IRepository<T> where T : class
+    {
+        IQueryable<T> All();
+
+        T GetById(int id);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
+
+        void Delete(int id);
+
+        void Detach(T entity);
+
+        int SaveChanges();
+    }
+}
--- Server/BullsAndCows.Web/Controllers/AccountController.cs
+++ Server/BullsAndCows.Web/Controllers/AccountController.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
-using BullsAndCows.Web.Models;
-using BullsAndCows.Models;
-
-namespace BullsAndCows.Web.Controllers
+﻿namespace BullsAndCows.Web.Controllers
 {
+    using BullsAndCows.Models;
+    using BullsAndCows.Web.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.Owin;
+    using Microsoft.Owin.Security;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using System.Web.Mvc;
+
     [Authorize]
     public class AccountController : Controller
     {
