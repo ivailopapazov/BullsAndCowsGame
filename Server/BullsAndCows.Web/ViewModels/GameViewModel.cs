63db92563dd40f6cec2dd798c5d815bf66574f14
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2018-04-20 00:06:45 +0300

    Add client notification when number contains same digits

--- Server/BullsAndCows.Web/ViewModels/GameViewModel.cs
+++ Server/BullsAndCows.Web/ViewModels/GameViewModel.cs
@@ -13,6 +13,7 @@
         [MaxLength(4, ErrorMessage = "Number must contain 4 digits!")]
         [DistinctCharacters(ErrorMessage = "All digits must be distinct!")]
         [RegularExpression("^[1-9]*$", ErrorMessage = "Use only digits from 1 to 9")]
+        [Display(Name = "Secret Number")]
         public string PlayerNumber { get; set; }
 
         public DateTime DateCreated { get; set; }
